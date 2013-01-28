c72e7091938620ffc6f0db70e4bf53e814d41253
Th4nat0s <thanspam@trollprod.org>
2013-01-28 22:21:47 +0100

    fix summarize

--- tshark/ddos_http.lua
+++ tshark/ddos_http.lua
@@ -4,6 +4,9 @@
 -- declare the tap
 tap_http_rq = nil
 
+-- declare max client output
+WINNERS = 15
+
 -- declare ip list array
 IP = {} 
 VALUE = nil
@@ -16,10 +19,35 @@ tap_http_rq = Listener.new(nil,"http.request")
 -- will get the parsed header
 client_f = Field.new("ip.src")
 
+-- Print the most significants requests
+function PRINT_MAX(TABLE)
+	-- For numer of winners
+	for I = 1,WINNERS do
+    	local MAX = 0
+        local MAXDATA = nil
+        -- Find the query with the Max errors
+        for DATA,COUNT in pairs(TABLE) do
+            if COUNT > MAX then
+                MAXDATA = DATA
+				MAX = COUNT 
+            end
+        end
+        -- If we can't find anything more
+        if MAX == 0 then
+            debug ( "No more winners" )
+            break
+        end
+        -- Print out and remove this item
+        debug ( MAX .. "," .. MAXDATA)
+        TABLE[MAXDATA]=0
+    end
+end
+
+
 -- this function is called at the end of the capture
 function tap_http_rq.draw()
-	debug("Request Http Stats: " )
-	for VALUE,COUNT in pairs(IP) do debug(COUNT .. "," .. VALUE) end
+	debug("Request Http Stats: (Total Hits in cap) " )
+	PRINT_MAX(IP)	
 end
 
 -- theses function are called each time the filter of the tap matches
